f21cdc005630c4b7bc9c89ce15001b1dfc2b1dff
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2015-03-14 13:22:15 +0900

    update version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools  import setup, find_packages
 
+version = '0.2.1'
+
 setup(name='python-voicetext',
-      version='0.2',
+      version=version,
       license='Apache License 2.0',
       description='Voice synthesiser using VoiceText Web API',
       long_description='See also https://github.com/youtalk/python-voicetext#readme',
@@ -10,7 +12,7 @@ setup(name='python-voicetext',
       author_email='yutaka.kondo@youtalk.jp',
       url='https://github.com/youtalk/python-voicetext',
       packages=find_packages(),
-      download_url='https://github.com/youtalk/python-voicetext/releases/tag/0.2',
+      download_url='https://github.com/youtalk/python-voicetext/releases/tag/' + version,
       install_requires=['requests', 'PyAudio'],
       platforms = ['POSIX', 'Mac OS X', 'Windows'],
       classifiers=[
